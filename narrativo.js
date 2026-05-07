6f90170a90a2b0c9a6af4bd8d537d3064355e38a
Juan Rozas <juanbr.rozas2005@gmail.com>
2026-05-06 21:25:19 -0300

    jogo

---
+++ narrativo.js
@@ -0,0 +1,14 @@
+/**
+ * Calculador Js - Exemplo de uso de funções
+ * @author Juan Rozas
+ */
+ 
+// importação de pacote
+const prompt = require('prompt-sync')()
+
+// variaveis
+let porta
+
+function escapar(){
+    
+}
